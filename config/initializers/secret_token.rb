80e989c8ecb627151b1fc06aebe4ea4020801491
maxim <mymailforxperia@gmail.com>
2012-11-19 21:54:19 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '9607c0fe849b1f982b4f3617d0f8d531560183f6f51398ad9a19e65592979e4217a5b9d9b44ce59d7953d7a81d7a376fc4759211b2b84db0862e331872cb5c01'
